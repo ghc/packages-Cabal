ba8d21b2869d9a692b988a2f2f52730ff775773a
David Eichmann <EichmannD@gmail.com>
2019-01-09 13:05:44 -0700

    extra-bundled-libraries: align naming convention with ghc

--- Cabal/Distribution/Simple/GHC.hs
+++ Cabal/Distribution/Simple/GHC.hs
@@ -109,12 +109,13 @@ import Distribution.Types.UnqualComponentName
 import Distribution.Utils.NubList
 import Language.Haskell.Extension
 
-import Control.Monad (msum)
+import Control.Monad (msum, forM_)
+import Data.List (isPrefixOf)
 import Data.Char (isLower)
 import qualified Data.Map as Map
 import System.Directory
          ( doesFileExist, getAppUserDataDirectory, createDirectoryIfMissing
-         , canonicalizePath, removeFile, renameFile )
+         , canonicalizePath, removeFile, renameFile, listDirectory )
 import System.FilePath          ( (</>), (<.>), takeExtension
                                 , takeDirectory, replaceExtension
                                 ,isRelative )
@@ -1829,7 +1830,10 @@ installLib verbosity lbi targetDir dynlibTargetDir _builtDir _pkg lib clbi = do
   -- copy the built library files over:
   whenHasCode $ do
     whenVanilla $ do
-      sequence_ [ installOrdinary builtDir targetDir       (mkGenericStaticLibName (l ++ f))
+      sequence_ [ installOrdinary
+                    builtDir
+                    targetDir
+                    (mkGenericStaticLibName (l ++ f))
                 | l <- getHSLibraryName (componentUnitId clbi):(extraBundledLibs (libBuildInfo lib))
                 , f <- "":extraLibFlavours (libBuildInfo lib)
                 ]
@@ -1837,11 +1841,31 @@ installLib verbosity lbi targetDir dynlibTargetDir _builtDir _pkg lib clbi = do
     whenProf $ do
       installOrdinary builtDir targetDir profileLibName
       whenGHCi $ installOrdinary builtDir targetDir ghciProfLibName
-    whenShared  $
-      sequence_ [ installShared builtDir dynlibTargetDir
-                    (mkGenericSharedLibName platform compiler_id (l ++ f))
-                | l <- getHSLibraryName uid : extraBundledLibs (libBuildInfo lib)
-                , f <- "":extraDynLibFlavours (libBuildInfo lib)
+    whenShared  $ do
+      sequence_ [ installShared
+                      builtDir
+                      dynlibTargetDir
+                      (mkGenericSharedLibName
+                        platform
+                        compiler_id
+                        (getHSLibraryName uid ++ f))
+                | f <- "":extraDynLibFlavours (libBuildInfo lib)
+                ]
+      sequence_ [ do
+                  files <- listDirectory builtDir
+                  let l' = mkGenericSharedBundledLibName
+                            platform
+                            compiler_id
+                            l
+                  forM_ files $ \ file ->
+                    when (l' `isPrefixOf` file) $ do
+                      isFile <- doesFileExist (builtDir </> file)
+                      when isFile $ do
+                        installShared
+                          builtDir
+                          dynlibTargetDir
+                          file
+                | l <- extraBundledLibs (libBuildInfo lib)
                 ]
 
   where
